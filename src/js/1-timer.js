0f1a1251d121a93fc124110f018767acf7713f30
Yuliia_Shefner <yul.she91@gmail.com>
2025-09-16 13:30:05 +0200

    added part of timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,32 @@
-console.log('Timer');
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const input = document.querySelector('#datetime-picker');
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const dateNow = Date.now();
+    if (selectedDate <= dateNow){
+        iziToast.warning({
+    title: 'Caution',
+    message: 'You forgot important data',
+});
+        startBtn.disabled = true;
+    } else {
+        startBtn.disabled = false;
+    }
+  },
+}; 
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn);
+
+flatpickr(input, options);
+
+let userSelectedDate = selectedDate;
